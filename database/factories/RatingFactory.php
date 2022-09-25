1ae34bbaf79dc5c8426c9ce196283c8dcc1fb35d
Henrique Foletto <henriquevidalfoletto@gmail.com>
2022-09-25 14:47:23 -0300

    Add rating Factory and Seeder

---
+++ database/factories/RatingFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Auditorium;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rating>
+ */
+class RatingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'auditorium_id' => Auditorium::factory(),
+            'image_rating' => fake()->optional()->numberBetween(1, 5),
+            'audio_rating' => fake()->optional()->numberBetween(1, 5),
+            'comfort_rating' => fake()->optional()->numberBetween(1, 5),
+            'bomboniere_rating' => fake()->optional()->numberBetween(1, 5),
+            'experience_rating' => fake()->optional()->numberBetween(1, 5),
+            'review' => fake()->optional()->paragraphs(rand(1, 5), true),
+            'visited_at' => fake()->date(),
+            'movie_watched' => fake()->word(),
+            'seat' => fake()->optional()->regexify('[A-O]{1}[0-2]{1}[0-9]{1}'),
+            'seat_rating' => fake()->optional()->numberBetween(1, 5),
+        ];
+    }
+}
---
+++ database/seeders/RatingSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Auditorium;
+use App\Models\Rating;
+use App\Models\Theater;
+use App\Models\User;
+use Faker\Generator;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+
+class RatingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Generator::class);
+        $user_ids = User::all()->pluck('id');
+        Auditorium::all()->each(function (Auditorium $auditorium) use ($faker, $user_ids) {
+            Rating::factory()
+                ->count($faker->numberBetween(0, 20))
+                ->for($auditorium)
+                ->sequence(fn (Sequence $sequence) => ['user_id' => $user_ids->random()])
+                ->create();
+        });
+    }
+}
