b7b81ff135f870df07c9f06330221ccaedf06e2c
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-01-31 00:52:08 -0300

    Update Theater seeder to use cities

--- database/seeders/TheaterSeeder.php
+++ database/seeders/TheaterSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
 use App\Models\Theater;
 use App\Models\TheaterChain;
 use Faker\Generator;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class TheaterSeeder extends Seeder
@@ -17,10 +19,19 @@ class TheaterSeeder extends Seeder
     public function run()
     {
         $faker = app(Generator::class);
+        $cities = City::all();
 
         TheaterChain::all()->each(fn (TheaterChain $theaterChain) => Theater::factory()
                 ->count($faker->numberBetween(2, 12))
                 ->for($theaterChain)
+                ->state(new Sequence(function ($sequence) use ($cities, $theaterChain, $faker) {
+                    $city = $cities->random();
+
+                    return [
+                        'city_id' => $city,
+                        'name' => "$theaterChain->name ".ucfirst($faker->word)." $city->name",
+                    ];
+                }))
                 ->create()
         );
     }
