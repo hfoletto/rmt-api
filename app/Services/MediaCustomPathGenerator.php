68b97d40f1014a70991ec80458147bfc17f668d7
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-02-12 11:15:03 -0300

    Add MediaCustomPathGenerator and add to media-library config

---
+++ app/Services/MediaCustomPathGenerator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Movie;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class MediaCustomPathGenerator implements PathGenerator
+{
+    /*
+     * Get the path for the given media, relative to the root storage path.
+     */
+    public function getPath(Media $media): string
+    {
+        return $this->getBasePath($media).'/';
+    }
+
+    /*
+     * Get the path for conversions of the given media, relative to the root storage path.
+     */
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->getBasePath($media).'/conversions/';
+    }
+
+    /*
+     * Get the path for responsive images of the given media, relative to the root storage path.
+     */
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getBasePath($media).'/responsive-images/';
+    }
+
+    /*
+     * Get a unique base path for the given media.
+     */
+    protected function getBasePath(Media $media): string
+    {
+        $prefix = config('media-library.prefix', '');
+
+        if ($prefix !== '') {
+            return $prefix . '/' . $this->getKey($media);
+        }
+
+        return $this->getKey($media);
+    }
+
+    protected function getKey(Media $media): string
+    {
+        if ($media->model_type === Movie::class) {
+            return $media->model->tmdb_id;
+        }
+        return $media->getKey();
+    }
+}
