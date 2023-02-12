b33dcd82198e9a4c545fdd8f14023719eda70c21
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-02-12 20:09:59 -0300

    Adjust AuditoriumFactory to add featured_image

--- database/factories/AuditoriumFactory.php
+++ database/factories/AuditoriumFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Auditorium;
 use App\Models\Theater;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Auditorium>
@@ -20,6 +22,22 @@ class AuditoriumFactory extends Factory
         return [
             'theater_id' => Theater::factory(),
             'name' => fake()->word(),
+            'description' => fake()->sentence(rand(12, 48)),
         ];
     }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Auditorium $auditorium) {
+
+            $images = Storage::disk('resources')->allFiles('img/auditoriums');
+            $random_image = $images[rand(0, count($images) - 1)];
+
+            $auditorium
+                ->addMedia(resource_path($random_image))
+                ->preservingOriginal()
+                ->toMediaCollection('featured_image');
+        });
+    }
+
 }
