813d27a4451c5ee59011d167c6801a56c693aaed
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-02-12 11:31:07 -0300

    Add php-tmdb/api package and create config and TmdbClient service

---
+++ app/Services/TmdbClient.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use Tmdb\Client;
+use Tmdb\Event\BeforeRequestEvent;
+use Tmdb\Event\Listener\Request\AcceptJsonRequestListener;
+use Tmdb\Event\Listener\Request\ApiTokenRequestListener;
+use Tmdb\Event\Listener\Request\ContentTypeJsonRequestListener;
+use Tmdb\Event\Listener\Request\UserAgentRequestListener;
+use Tmdb\Event\Listener\RequestListener;
+use Tmdb\Event\RequestEvent;
+use Tmdb\Token\Api\ApiToken;
+
+class TmdbClient
+{
+
+    static public function getClient(): Client {
+        $token = new ApiToken(config('tmdb.api_key'));
+
+        $ed = new \Symfony\Component\EventDispatcher\EventDispatcher();
+
+        $client = new Client(
+            [
+                'api_token' => $token,
+                'event_dispatcher' => [
+                    'adapter' => $ed
+                ],
+                // We make use of PSR-17 and PSR-18 auto discovery to automatically guess these, but preferably set these explicitly.
+                'http' => [
+                    'client' => null,
+                    'request_factory' => null,
+                    'response_factory' => null,
+                    'stream_factory' => null,
+                    'uri_factory' => null,
+                ]
+            ]
+        );
+
+        /**
+         * Required event listeners and events to be registered with the PSR-14 Event Dispatcher.
+         */
+        $requestListener = new RequestListener($client->getHttpClient(), $ed);
+        $ed->addListener(RequestEvent::class, $requestListener);
+
+        $apiTokenListener = new ApiTokenRequestListener($client->getToken());
+        $ed->addListener(BeforeRequestEvent::class, $apiTokenListener);
+
+        $acceptJsonListener = new AcceptJsonRequestListener();
+        $ed->addListener(BeforeRequestEvent::class, $acceptJsonListener);
+
+        $jsonContentTypeListener = new ContentTypeJsonRequestListener();
+        $ed->addListener(BeforeRequestEvent::class, $jsonContentTypeListener);
+
+        $userAgentListener = new UserAgentRequestListener();
+        $ed->addListener(BeforeRequestEvent::class, $userAgentListener);
+
+        return $client;
+
+    }
+
+}
