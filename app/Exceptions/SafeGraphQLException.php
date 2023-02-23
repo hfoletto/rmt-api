10721c03af77432e0a4695698c9cae321abbe318
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-02-23 20:19:58 -0300

    Remove error extension field category in SafeGraphQLException

--- app/Exceptions/SafeGraphQLException.php
+++ app/Exceptions/SafeGraphQLException.php
@@ -26,20 +26,6 @@ class SafeGraphQLException extends Exception implements ClientAware, ProvidesExt
     }
 
     /**
-     * Returns string describing a category of the error.
-     *
-     * Value "graphql" is reserved for errors produced by query parsing or validation, do not use it.
-     *
-     * @api
-     *
-     * @return string
-     */
-    public function getCategory(): string
-    {
-        return 'custom';
-    }
-
-    /**
      * Return the content that is put in the "extensions" part
      * of the returned error.
      *
